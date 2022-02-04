f210f892e4b603086e81feeb91b3db4847c80176
Parmeshramsekar <parmesh.02.03@gmail.com>
2022-02-04 10:39:43 +0530

    Delete wifi.py

--- wifi.py
+++
@@ -1,6 +0,0 @@
-import subprocess
-
-data = subprocess.check_output(['netsh','wlan','show','profiles']).decode()
-profiles = [i.split(":")[1][1:-1]for i in data if "All user profile"in i]
-for i in profiles:
-    results = subprocess.check_output(['netsh','wlan','show','profile',i,])
